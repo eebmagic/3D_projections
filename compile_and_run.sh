715f3857803b8d945dc39f7a69bb5557b4c5f06d
eebmagic <eebmagic@gmail.com>
2020-06-10 18:14:31 -0500

    edited compile script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,4 @@
 echo "COMPILING:"
 javac --module-path ./dependancies/javafx-sdk-11.0.2/lib/ --add-modules=javafx.controls Program.java
 echo "\n\nRUNNING:"
-java --module-path ./dependancies/javafx-sdk-11.0.2/lib/ --add-modules=javafx.controls Program.java
+java --module-path ./dependancies/javafx-sdk-11.0.2/lib/ --add-modules=javafx.controls Program
